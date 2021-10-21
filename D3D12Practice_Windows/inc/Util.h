8fdb2d4348521e68d9f334be04222e6fa5ea51c8
Kenny91 <odessa5d4@gmail.com>
2021-10-21 14:13:20 +0900

    Some util updated.

--- D3D12Practice_Windows/inc/Util.h
+++ D3D12Practice_Windows/inc/Util.h
@@ -15,27 +15,63 @@ struct FenceObject
 
 struct RootParameter
 {
-
-	std::unique_ptr<D3D12_ROOT_PARAMETER> Parameter;
-	std::unique_ptr<D3D12_ROOT_DESCRIPTOR_TABLE> Table;
-	std::unique_ptr<D3D12_DESCRIPTOR_RANGE> Range;
+	std::shared_ptr<D3D12_ROOT_PARAMETER> Parameter;
+	std::shared_ptr<D3D12_ROOT_DESCRIPTOR_TABLE> Table;
+	std::shared_ptr<D3D12_DESCRIPTOR_RANGE> Range;
 
 	uint DescriptorCount;
 	D3D12_DESCRIPTOR_RANGE_TYPE Type;
 
-	RootParameter(uint descriptorCount, D3D12_DESCRIPTOR_RANGE_TYPE type)
+	RootParameter(uint descriptorCount, D3D12_DESCRIPTOR_RANGE_TYPE type, D3D12_SHADER_VISIBILITY visibility) noexcept
 		: DescriptorCount(descriptorCount), Type(type),
-		Parameter(std::make_unique<D3D12_ROOT_PARAMETER>()),
-		Table(std::make_unique<D3D12_ROOT_DESCRIPTOR_TABLE>()),
-		Range(std::make_unique<D3D12_DESCRIPTOR_RANGE>())
+		Parameter(std::make_shared<D3D12_ROOT_PARAMETER>()),
+		Table(std::make_shared<D3D12_ROOT_DESCRIPTOR_TABLE>()),
+		Range(std::make_shared<D3D12_DESCRIPTOR_RANGE>())
+	{
+		Parameter->ParameterType = D3D12_ROOT_PARAMETER_TYPE_DESCRIPTOR_TABLE;
+		Parameter->ShaderVisibility = visibility;
+
+		Range->NumDescriptors = descriptorCount;
+		Range->RangeType = type;
+		Range->OffsetInDescriptorsFromTableStart = D3D12_DESCRIPTOR_RANGE_OFFSET_APPEND;
+
+
+		Table->NumDescriptorRanges = 1;
+		Table->pDescriptorRanges = Range.get();
+
+
+		Parameter->DescriptorTable = *Table;
+	}
+
+	RootParameter()
+		: DescriptorCount(0), Type(D3D12_DESCRIPTOR_RANGE_TYPE_SRV)
 	{
 
 	}
 
-	RootParameter(const RootParameter& other) = delete;
-	RootParameter(RootParameter&& other)
-		:	Parameter(std::move(other.Parameter)), Table(std::move(other.Table)), Range(std::move(other.Range))
-			, DescriptorCount(other.DescriptorCount), Type(other.Type)
+	RootParameter(const RootParameter& other)
+		: Parameter(other.Parameter), Table(other.Table), Range(other.Range)
+		, DescriptorCount(other.DescriptorCount), Type(other.Type)
+	{
+
+	}
+
+	RootParameter operator=(const RootParameter& other)
+	{
+		Parameter = other.Parameter;
+		Table = other.Table;
+		Range = other.Range;
+
+		DescriptorCount = other.DescriptorCount;
+		Type = other.Type;
+
+		return *this;
+	}
+
+
+	RootParameter(RootParameter&& other) noexcept
+		: Parameter(std::move(other.Parameter)), Table(std::move(other.Table)), Range(std::move(other.Range))
+		, DescriptorCount(other.DescriptorCount), Type(other.Type)
 	{
 
 	}
@@ -83,7 +119,7 @@ static FenceObject MakeFence(ID3D12Device4* device, D3D12_FENCE_FLAGS flags = D3
 	return fence;
 }
 
-static D3D12_BLEND_DESC MakeBlendState()
+static D3D12_BLEND_DESC MakeDefaultBlendState()
 {
 	D3D12_BLEND_DESC blendDesc{};
 	D3D12_RENDER_TARGET_BLEND_DESC rtBlendDesc{};
@@ -135,19 +171,48 @@ static D3D12_CPU_DESCRIPTOR_HANDLE MakeCPUDescriptorHandle(ID3D12DescriptorHeap*
 }
 
 
-static const RootParameter&& MakeRootParameter(uint descriptorCount, D3D12_DESCRIPTOR_RANGE_TYPE type)
+//static RootParameter&& MakeRootParameter(uint descriptorCount, D3D12_DESCRIPTOR_RANGE_TYPE type)
+//{
+//	RootParameter param(descriptorCount, type, );
+//
+//	param.Range->NumDescriptors = descriptorCount;
+//	param.Range->RangeType = type;
+//	param.Range->OffsetInDescriptorsFromTableStart = D3D12_DESCRIPTOR_RANGE_OFFSET_APPEND;
+//
+//	param.Table->NumDescriptorRanges = 1;
+//	param.Table->pDescriptorRanges = param.Range.get();
+//
+//	param.Parameter->DescriptorTable = *param.Table;
+//
+//	return std::move(param);
+//	
+//}
+
+static D3D12_RASTERIZER_DESC MakeDefaultRasterizerDescriptor()
 {
-	RootParameter param(descriptorCount, type);
+	D3D12_RASTERIZER_DESC rsDesc{};
 
-	param.Range->NumDescriptors = descriptorCount;
-	param.Range->RangeType = type;
-	param.Range->OffsetInDescriptorsFromTableStart = D3D12_DESCRIPTOR_RANGE_OFFSET_APPEND;
+	rsDesc.CullMode = D3D12_CULL_MODE_NONE;
+	rsDesc.FillMode = D3D12_FILL_MODE_SOLID;
+	rsDesc.DepthClipEnable = true;
+	rsDesc.ConservativeRaster = D3D12_CONSERVATIVE_RASTERIZATION_MODE_OFF;
 
-	param.Table->NumDescriptorRanges = 1;
-	param.Table->pDescriptorRanges = param.Range.get();
+	return rsDesc;
+}
 
-	param.Parameter->DescriptorTable = *param.Table;
-
-	return std::move(param);
+static D3D12_RESOURCE_DESC MakeTexture2DDescriptor(uint width, uint height, DXGI_FORMAT format)
+{
+	D3D12_RESOURCE_DESC texDesc{};
 	
+	texDesc.Format = format;
+	texDesc.DepthOrArraySize = 1;
+	texDesc.Width = width;
+	texDesc.Height = height;
+	texDesc.Dimension = D3D12_RESOURCE_DIMENSION_TEXTURE2D;
+	texDesc.Layout = D3D12_TEXTURE_LAYOUT_UNKNOWN;
+	texDesc.SampleDesc.Count = 1;
+	texDesc.MipLevels = 1;
+
+	return texDesc;
+
 }
